d652d4fdcee36594b93620ccc9b1c7de40ce6d4b
guyulongcs <guyulongcs@gmail.com>
2020-09-07 01:44:57 +0800

    update

--- generateReadme.py
+++ generateReadme.py
@@ -9,9 +9,11 @@ __author__ = 'Yulong Gu'
 paper_class_map = {}
 paper_map = {}
 
-file_object = open('./README.md')
-all_lines = file_object.readlines()
-file_object.close()
+all_lines = []
+if(os.path.exists('./README.md')):
+    file_object = open('./README.md')
+    all_lines = file_object.readlines()
+    file_object.close()
 
 out_file = open('./README.md', 'w')
 
@@ -24,7 +26,7 @@ catalog_flag = 0
 for line in all_lines:
     if catalog_flag != 1:
         out_file.write(line)
-    if line.startswith("##"):
+    if line.startswith("#"):
         catalog_flag = 1
 
     if paper_class_flag == 1 and not line.startswith("*") and not line.startswith("#"):
@@ -43,23 +45,48 @@ for line in all_lines:
             paper_flag = 1
             paper_name = line[line.find("[")+1:line.find("]")].strip()
 
-    if line.startswith("###"):
+    if line.startswith("##"):
         paper_class_flag = 1
         paper_class_name = line[3:].strip()
 
 github_root = "https://github.com/guyulongcs/Deep-Learning-for-Search-Recommendation-Advertisements/blob/master/"
 all_dir = os.listdir("./")
+all_dir.sort()
 for one_dir in all_dir:
     if os.path.isdir(one_dir) and not one_dir.startswith('.'):
-        out_file.write("\n### " + one_dir+"\n")
+        out_file.write("\n## " + one_dir+"\n")
         if one_dir.strip() in paper_class_map:
             out_file.write(paper_class_map[one_dir.strip()] + "\n")
         all_sub_files = os.listdir(one_dir)
+        all_sub_files.sort()
         for one_file in all_sub_files:
-            if not os.path.isdir(one_file) and not one_file.startswith('.'):
-                out_file.write("* [" + ('.').join(one_file.split('.')[:-1]) + "]("+github_root + urllib.quote(one_dir.strip())+"/"
-                               + urllib.quote(one_file.strip())+") <br />\n")
+            print("one_file:%s, is_dir:%d" % (one_file, os.path.isdir(os.path.join(one_dir, one_file))))
+            one_file_2=os.path.join(one_dir, one_file)
+            if not os.path.isdir(one_file_2) and not one_file.startswith('.'):
+                # out_file.write("* [" + ('.').join(one_file.split('.')[:-1]) + "]("+github_root 
+                #                + urllib.quote(one_file.strip())+") <br />\n")
+                out_file.write("* [" + ('.').join(one_file.split('.')[:-1]) + "]("+github_root + one_dir.strip()+"/"
+                               + one_file.strip()+") <br />\n")
                 if one_file.strip() in paper_map:
                     out_file.write(paper_map[one_file.strip()] + "\n")
+
+        for one_file in all_sub_files:
+            one_file_2=os.path.join(one_dir, one_file)
+            if os.path.isdir(one_file_2) and not one_file_2.startswith('.'):
+                one_dir_second = one_file_2
+                print("one_dir_second:",one_dir_second)
+                out_file.write("\n#### " + one_file +"\n")
+                all_sub_files_second = os.listdir(one_dir_second)
+                all_sub_files_second.sort()
+                for one_file_second in all_sub_files_second:
+                    print("one_file_second:",one_file_second)
+                    if not os.path.isdir(one_file_second) and not one_file_second.startswith('.'):
+                        # out_file.write("* [" + ('.').join(one_file_second.split('.')[:-1]) + "]("+github_root + urllib.quote(one_dir.strip())+"/"
+                        #         + urllib.quote(one_dir_second.strip())+"/"
+                        #        + urllib.quote(one_file_second.strip())+") <br />\n")
+                        out_file.write("* [" + ('.').join(one_file_second.split('.')[:-1]) + "]("+github_root 
+                                + one_dir_second.strip()+"/"
+                               + one_file_second.strip()+") <br />\n")
+
 
 out_file.close()
